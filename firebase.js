1d1f34bfe3fd9fcc20942fc9b266f25224ec4a66
JeffDing27 <rud300@ensign.edu>
2024-08-07 22:17:17 -0600

    first commit

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlpZuehpvyzwPoI91WwZ1GCMlR17ggshU",
+  authDomain: "inventory-managment-e8d1a.firebaseapp.com",
+  projectId: "inventory-managment-e8d1a",
+  storageBucket: "inventory-managment-e8d1a.appspot.com",
+  messagingSenderId: "1017352987930",
+  appId: "1:1017352987930:web:13be1317eceb0bd832b8a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export default firestore;
